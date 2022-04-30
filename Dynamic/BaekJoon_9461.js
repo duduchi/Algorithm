35d95e68815ca80cd4587dc7a9f73a56d269d568
Duchi <dlgmlrb7736@gmail.com>
2022-04-30 17:33:21 +0900

    백준 9461 문제 javascript 입력 받을 수 있도록 수정

--- Dynamic/BaekJoon_9461.js
+++ Dynamic/BaekJoon_9461.js
@@ -1,23 +1,40 @@
-const answer = (number) => {
-  const arr = Array.from({ length: number + 1 }, () => 0);
+const answer = ([...number]) => {
 
-  arr[1] = 1;
-  arr[2] = 1;
-  arr[3] = 1;
-  arr[4] = 2;
-  arr[5] = 2;
+  const arr = number.map(data => Number(data))
 
-  for (let index = 6; index < arr.length; index++) {
-    arr[index] = arr[index - 1] + arr[index - 5];
+  const order = arr[0];
+
+  let dp = [];
+
+  for (let index = 1; index <= order; index++) {
+    dp = Array.from({ length: arr[index] + 1 }, () => 0);
+
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    dp[4] = 2;
+    dp[5] = 2;
+
+    for (let index = 6; index < dp.length; index++) {
+      dp[index] = dp[index - 1] + dp[index - 5];
+    }
+
+    console.log(dp[arr[index]])
   }
-
-console.log(arr);
-
-  return arr[number];
 };
 
-console.log(answer(6));
-console.log(answer(12));
+
+const input = [];
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", (line) => {
+    input.push(line);
+  })
+  .on("close", () => {
+    // console.log(input)
+    answer(input);
+    process.exit();
+  });
 
 // https://www.acmicpc.net/problem/9461
 // 다이내믹 프로그래밍은 점화식을 찾아서 리스트화 하는게 핵심
