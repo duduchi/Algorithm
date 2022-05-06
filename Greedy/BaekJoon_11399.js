48f0354931ef6a169a0f86e0b0e3249dd50bf752
Duchi <dlgmlrb7736@gmail.com>
2022-05-06 22:44:42 +0900

    11399 문제 첫번째 문제 풀이로 주석 변경

--- Greedy/BaekJoon_11399.js
+++ Greedy/BaekJoon_11399.js
@@ -1,23 +1,25 @@
 const answer = ([n, arr]) => {
     const time = arr.split(' ').map(Number).sort((a, b) => a - b);
 
-    // const totalTime = [];
-    // let sumTime = 0;
-    // for (let index = 0; index < time.length; index++) {
-    //     sumTime += time[index];
-    //     totalTime.push(sumTime);
-    // }
-
-    // console.log(totalTime.reduce((a,b) => a+b,0));
-
+    // 첫번째 풀이
+    const totalTime = [];
     let sumTime = 0;
     for (let index = 0; index < time.length; index++) {
-        for (let index2 = 0; index2 < index + 1; index2++) {
-            sumTime += time[index2]
-        }        
+        sumTime += time[index];
+        totalTime.push(sumTime);
     }
 
-    console.log(sumTime);
+    console.log(totalTime.reduce((a,b) => a+b,0));
+
+    // // 두번째 풀이
+    // let sumTime = 0;
+    // for (let index = 0; index < time.length; index++) {
+    //     for (let index2 = 0; index2 < index + 1; index2++) {
+    //         sumTime += time[index2]
+    //     }        
+    // }
+
+    // console.log(sumTime);
 }
 
 const input = [];
