145536a8d260221b6ec8ae2b2e59e03555d65e83
Duchi <dlgmlrb7736@gmail.com>
2022-05-06 22:42:30 +0900

    백준 11399 탐욕 알고리즘 문제 풀이

---
+++ Greedy/BaekJoon_11399.js
@@ -0,0 +1,32 @@
+const answer = ([n, arr]) => {
+    const time = arr.split(' ').map(Number).sort((a, b) => a - b);
+
+    // const totalTime = [];
+    // let sumTime = 0;
+    // for (let index = 0; index < time.length; index++) {
+    //     sumTime += time[index];
+    //     totalTime.push(sumTime);
+    // }
+
+    // console.log(totalTime.reduce((a,b) => a+b,0));
+
+    let sumTime = 0;
+    for (let index = 0; index < time.length; index++) {
+        for (let index2 = 0; index2 < index + 1; index2++) {
+            sumTime += time[index2]
+        }        
+    }
+
+    console.log(sumTime);
+}
+
+const input = [];
+require("readline")
+    .createInterface(process.stdin, process.stdout)
+    .on("line", (line) => {
+        input.push(line);
+    })
+    .on("close", () => {
+        answer(input);
+        process.exit();
+    });
