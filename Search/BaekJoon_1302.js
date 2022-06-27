34f3464f11df91155cc854803b60ec0cc534906b
Duchi <dlgmlrb7736@gmail.com>
2022-06-28 07:28:46 +0900

    1302번 문제 주석 추가

--- Search/BaekJoon_1302.js
+++ Search/BaekJoon_1302.js
@@ -3,18 +3,20 @@ const answer = ([input,...n]) => {
 
     const books = n.sort();
 
-    let maxCnt = 0;
-    let nowCnt = 0;
-    let maxBook = '';
-    let nowBook = books[0];
+    let maxCnt = 0; // 가장 많이 팔린 책의 수
+    let nowCnt = 0; // 현재 검색하고 있는 책의 수
+    let maxBook = ''; // 가장 많이 팔린 책의 이름
+    let nowBook = books[0]; // 비교를 위하여 책의 첫번째 요소를 할당
 
     books.forEach(data => {
+        // 현재의 책과 과거의 책이 같은면 카운트 + 1 / 같이 않으면 카운트 = 1
         if(nowBook === data){
             nowCnt += 1;
         }else{
             nowCnt = 1;
         }
 
+        // 현재의 카운트가 가장 많이 팔린 책의 수 보다 크면 가장 많이 팔린 책의 이름과 카운트를 변경
         if(maxCnt < nowCnt){
             maxBook = nowBook
             maxCnt = nowCnt
