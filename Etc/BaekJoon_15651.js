ca2d9595cc7ebfc0e60fe30761c3ed5c36f894c1
Duchi <dlgmlrb7736@gmail.com>
2022-08-01 06:49:20 +0900

    15651 출력 방식 수정

--- Etc/BaekJoon_15651.js
+++ Etc/BaekJoon_15651.js
@@ -1,12 +1,19 @@
+/*
+1. 아이디어
+15649 문제에서 중복 체크 로직을 제거하면 된다.
+baekjoon 제출에서 수가 큰 반복적인 로직에서 console.log가 시간 초과를 야기할 수 있으므로, 변수를 지정하여 마지막에 찍도록 수정.
+*/
 const answer = ([data]) => {
 
     const [n,m] = data.split(' ').map(Number);
 
     const rs = [];
+    let result = '';
 
     const setData = (num) => {
         if(num === m){
-            console.log(rs.join(' '));
+            // console.log(rs.join(' '));
+            result += `${rs.join(' ')}\n`
             return;
         }
         for (let index = 1; index <= n; index++) {
@@ -17,6 +24,7 @@ const answer = ([data]) => {
     }
 
     setData(0);
+    console.log(result.trim())
 
 }
 
