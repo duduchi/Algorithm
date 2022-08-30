1483d20898777c62510ec386f0de4ff51e766eb4
Duchi <dlgmlrb7736@gmail.com>
2022-08-31 06:51:22 +0900

    동적 계획법 1912 연속합 문제 풀이

--- Dynamic/BaekJoon_1912.js
+++ Dynamic/BaekJoon_1912.js
@@ -1,30 +1,22 @@
 /*
 연속된 수 끼리만 더할 수 있다.
-시작값부터 하나씩 더해 가며 max값을 갱신하고, max값 보다 작을 때에는 다음 턴으로 넘어간다.
+list 이번값과 dp 이전값을 더하였을 때 dp의 이전값보다 크면 더하는 방식을 반복하여 연속된 수 사이에서 계속 더하기를 반복해 나간다.
 */
 
 const answer = ([n,data]) => {
     const list = data.split(' ').map(Number);
-    const arr = Array.from({length : n},()=>[])
 
-    list.forEach((value,index) => {
-       arr[index][0] = value; 
-    });
+    const dp = [];
+    for (let index = 0; index < Number(n); index++) {
+        dp[index] = list[index]
 
-    for (let i = 0; i < list.length; i++) {
-        for (let j = i+1; j < list.length; j++) {
-            arr[i].push(arr[i].at(-1) + list[j]);
+        if(dp[index] < dp[index-1] + list[index]){
+            dp[index] = dp[index-1] + list[index]
         }
     }
 
-    console.log(arr)
+    console.log(Math.max(...dp))
 
-    const rs = []
-    for (let index = 0; index < arr.length; index++) {
-        rs.push(Math.max(...arr[index]))
-    }
-
-    console.log(Math.max(...rs));
 }
 
 const input = [];
